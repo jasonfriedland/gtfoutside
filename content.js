798dee60f4b312d2deda6d7e3f7b68a05739e6cf
Jason Friedland <jason@friedland.id.au>
2018-06-06 12:06:41 +1000

    Initial commit

---
+++ content.js
@@ -0,0 +1,34 @@
+// Hides "rode on Zwift" activities on Strava.
+
+function gtfoutside() {
+    console.log("Running GTFOutside")
+    var activities = document.getElementsByClassName("activity");
+    for (var i = 0; i < activities.length; i++) {
+        for (var j = 0; j < activities[i].childNodes.length; j++) {
+            if (activities[i].childNodes[j].className == "entry-header") {
+                if (activities[i].childNodes[j].innerText.indexOf("rode on Zwift") !== -1) {
+                    console.log("Found a Zwift, hiding ActivityID: " + activities[i].id)
+                    activities[i].style.display = "none";
+                    break;
+                }
+            }
+        }
+    }
+}
+
+function init() {
+    var button = document.createElement("button");
+    button.id = "GTFOutside";
+    button.textContent = "gtfoutside"
+    button.className = "button btn btn-sm btn-primary"
+    button.style.fontSize = "12px"
+    button.addEventListener("click", gtfoutside, false);
+
+    var uploadMenu = document.getElementsByClassName("upload-menu")[0];
+    uploadMenu.parentNode.insertBefore(button, uploadMenu.nextSibling)
+
+    console.log("Initialised GTFOutside")
+    window.addEventListener("load", gtfoutside, false);
+}
+
+init();
