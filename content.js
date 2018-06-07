e459fbe37f8b68f6c1222d63e2b6d4d74e00ce0a
Jason Friedland <jason@friedland.id.au>
2018-06-07 11:56:01 +1000

    Minor JS lint, formatting

--- content.js
+++ content.js
@@ -1,16 +1,16 @@
 // Hides "rode on Zwift" activities on Strava.
 
 function gtfoutside() {
-    console.log("Running GTFOutside")
+    console.log("Running GTFOutside");
     var activities = document.getElementsByClassName("activity");
     for (var i = 0; i < activities.length; i++) {
         for (var j = 0; j < activities[i].childNodes.length; j++) {
-            if (activities[i].childNodes[j].className == "entry-header") {
-                if (activities[i].childNodes[j].innerText.indexOf("rode on Zwift") !== -1) {
-                    console.log("Found a Zwift, hiding ActivityID: " + activities[i].id)
-                    activities[i].style.display = "none";
-                    break;
-                }
+            var activityChild = activities[i].childNodes[j];
+            if (activityChild.className == "entry-header" &&
+                    activityChild.innerText.indexOf("rode on Zwift") !== -1) {
+                console.log("Found a Zwift, hiding ActivityID: " + activities[i].id);
+                activities[i].style.display = "none";
+                break;
             }
         }
     }
@@ -19,15 +19,15 @@ function gtfoutside() {
 function init() {
     var button = document.createElement("button");
     button.id = "GTFOutside";
-    button.textContent = "gtfoutside"
-    button.className = "button btn btn-sm btn-primary"
-    button.style.fontSize = "12px"
+    button.textContent = "gtfoutside";
+    button.className = "button btn btn-sm btn-primary";
+    button.style.fontSize = "12px";
     button.addEventListener("click", gtfoutside, false);
 
     var uploadMenu = document.getElementsByClassName("upload-menu")[0];
-    uploadMenu.parentNode.insertBefore(button, uploadMenu.nextSibling)
+    uploadMenu.parentNode.insertBefore(button, uploadMenu.nextSibling);
 
-    console.log("Initialised GTFOutside")
+    console.log("Initialised GTFOutside");
     window.addEventListener("load", gtfoutside, false);
 }
 
