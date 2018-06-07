a6b5aa567ea3dccb1ab4ac05d23ae3e09c90c506
Jason Friedland <jason@friedland.id.au>
2018-06-07 14:09:08 +1000

    Refactor into smaller functions for testing, spike hiding Trainerroad too

--- content.js
+++ content.js
@@ -1,19 +1,35 @@
 // Hides "rode on Zwift" activities on Strava.
 
+const HIDE_ZWIFT = true;
+const HIDE_TRAINERROAD = false;
+
 function gtfoutside() {
     console.log("Running GTFOutside");
     var activities = document.getElementsByClassName("activity");
+    hideZwift(activities)
+}
+
+function hideZwift(activities) {
     for (var i = 0; i < activities.length; i++) {
         for (var j = 0; j < activities[i].childNodes.length; j++) {
-            var activityChild = activities[i].childNodes[j];
-            if (activityChild.className == "entry-header" &&
-                    activityChild.innerText.indexOf("rode on Zwift") !== -1) {
+            var activityChildNode = activities[i].childNodes[j];
+            if (activityChildNode.className == "entry-header" && checkHideActivity(activityChildNode)) {
                 console.log("Found a Zwift, hiding ActivityID: " + activities[i].id);
                 activities[i].style.display = "none";
                 break;
             }
         }
     }
+}
+
+function checkHideActivity(elem) {
+    if (HIDE_ZWIFT && elem.innerText.indexOf("rode on Zwift") !== -1) {
+        return true;
+    }
+    if (HIDE_TRAINERROAD && elem.innerText.indexOf("rode on Trainerroad") !== -1) {
+        return true;
+    }
+    return false
 }
 
 function init() {
